ae400f85dd5f69752047076325c9d7ea07f1d008
Dailin Hu <213160842@seu.edu.cn>
2018-09-04 15:41:14 +0800

    remove qingdaooj frontend

--- COLOSSEUM/COLOSSEUM/settings.py
+++ COLOSSEUM/COLOSSEUM/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    # 'corsheaders',
     'rest_framework',
     'backend',
 ]
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'COLOSSEUM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['frontend/dist'],
+        'DIRS': ['frontend'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- COLOSSEUM/frontend/src_old/pages/admin/components/btn/Cancel.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <el-button plain type="primary">Cancel</el-button>
-</template>
-<script>
-  export default{
-    name: 'Cancel'
-  }
-</script>
